feb5e05db311382db2141cadf14022911595aa24
boticelli <kandelakitina@gmail.com>
2025-02-15 18:35:38 +0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { flakelight, ... }:
+    flakelight ./. {
+      devShell = {
+        packages = pkgs:
+          with pkgs;
+          with nodePackages; [
+            nodejs
+            live-server
+            pm2
+          ];
+        shellHook = ''
+          unset name
+          pm2 start live-server
+        '';
+      };
+    };
+}
